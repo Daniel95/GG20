41a38a3252033b815653c019405291bca7cdc297
idaniel95 <idaniel95@me.com>
2020-01-31 22:51:36 +0100

    added scripts for sword and taskmanager.

---
+++ GGJ20/Assets/Scripts/Goal/ShapingGoal.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ShapingGoal", menuName = "ScriptableObjects/Goals/ShapingGoal", order = 1)]
+public class ShapingGoal : ScriptableObject
+{
+    public int TargetHits;
+}
---
+++ GGJ20/Assets/Scripts/TaskManager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class TaskManager : MonoBehaviour
+{
+    private static TaskManager instance;
+
+    public static TaskManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = GameObject.FindObjectOfType<TaskManager>();
+            }
+
+            return instance;
+        }
+    }
+
+    [Category("Shaping")]
+    [SerializeField] [Range(1, 10)] private int MinTargetWidth;
+    [SerializeField] [Range(1, 10)] private int MaxTargetWidth;
+
+    [Category("Sharpening")]
+    [SerializeField] [Range(1, 10)] private int MinTargetSharpness;
+    [SerializeField] [Range(1, 10)] private int MaxTargetSharpness;
+
+    public static void CreateTask(out ShapingTask shapingTask)
+    {
+        shapingTask = new ShapingTask();
+        shapingTask.TargetWidth = Random.Range(Instance.MinTargetWidth, Instance.MaxTargetWidth);
+    }
+
+    public static void CreateTask(out SharpeningTask sharpeningTask)
+    {
+        sharpeningTask = new SharpeningTask();
+        sharpeningTask.TargetSharpness = Random.Range(Instance.MinTargetSharpness, Instance.MaxTargetSharpness);
+    }
+
+    public struct ShapingTask
+    {
+        public int TargetWidth;
+    }
+
+    public struct SharpeningTask
+    {
+        public int TargetSharpness;
+    }
+}
