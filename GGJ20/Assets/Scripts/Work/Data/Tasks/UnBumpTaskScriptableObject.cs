af0cbe64225a7bc0fa83ad02cca503275f2333f0
Max <max.mxmail@gmail.com>
2020-02-01 18:15:17 +0100

    UnBumbTask.

--- GGJ20/Assets/Scripts/Work/Data/Tasks/UnBumpTaskScriptableObject.cs
+++ GGJ20/Assets/Scripts/Work/Data/Tasks/UnBumpTaskScriptableObject.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "UnBumpingTask", menuName = "ScriptableObjects/UnBumpingTask", order = 1)]
 public class UnBumpTaskScriptableObject : TaskScriptableObject
 {
-    public int hits = 5;
+    [Tooltip("0 is perfect everything else is bumpy! It's from all the ripple effects combined. (Amplitude)")]
+    [Range(-10.0f, 10.0f)]
+    public float totalTargetRippleness = 0.0f;
 
     public override WorkManager.TaskType GetTaskType()
     {
