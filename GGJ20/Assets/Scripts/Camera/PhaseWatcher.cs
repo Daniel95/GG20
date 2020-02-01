25c74b738412963e990ac65db8d8a706dc0439f8
Wopsie <joaghim.beltman@gmail.com>
2020-02-01 12:45:16 +0100

    Switched  models for tasks in scene. Working on task behaviours

--- GGJ20/Assets/Scripts/Camera/PhaseWatcher.cs
+++ GGJ20/Assets/Scripts/Camera/PhaseWatcher.cs
@@ -16,6 +16,7 @@ public class PhaseWatcher : MonoBehaviour
     private void Start()
     {
         currHookIndx = 0;
+        prevHookIndx = cameraHooks.Count;
         if (cameraHooks.Count <= 0)
         {
             Debug.LogError("define camera hooks, you buffoon");
@@ -41,18 +42,17 @@ public class PhaseWatcher : MonoBehaviour
 
     public void NextPhase()
     {
+        Debug.Log("Next Phase");
         if((currHookIndx + 1) < cameraHooks.Count)
         {
             //safe
-            prevHookIndx = currHookIndx;
-            currHookIndx++;
+            GoToPhase(currHookIndx+1);
             fp = 0.1f;
         }
         else
         {
             //wrap to first phase
-            prevHookIndx = currHookIndx;
-            currHookIndx = 0;
+            GoToPhase(0);
             fp = 0.1f;
         }
     }
@@ -61,5 +61,8 @@ public class PhaseWatcher : MonoBehaviour
     {
         prevHookIndx = currHookIndx;
         currHookIndx = phaseIndex;
+
+        cameraHooks[prevHookIndx].GetComponentInParent<Task>().EndTask();
+        cameraHooks[currHookIndx].GetComponentInParent<Task>().StartTask();
     }
 }
---
+++ GGJ20/Assets/Scripts/Job/Task/Task.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Task : MonoBehaviour
+{
+    private bool isStarted = false;
+
+
+
+    /// <summary>
+    /// Mark this task as in progress, start animation etc.
+    /// </summary>
+    public void StartTask()
+    {
+        Debug.Log("Starting task: " + gameObject.name);
+        isStarted = true;
+    }
+
+    public void EndTask()
+    {
+        Debug.Log("Ending task: " + gameObject.name);
+        isStarted = true;
+    }
+
+}
