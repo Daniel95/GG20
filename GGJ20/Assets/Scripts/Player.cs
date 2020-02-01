dd68d7fce9f7c80c4bb5706c9af0e22e2d843122
Wopsie <joaghim.beltman@gmail.com>
2020-02-01 16:34:11 +0100

    Add camera slerping behaviour to player from PhaseWatcher

--- GGJ20/Assets/Scripts/Player.cs
+++ GGJ20/Assets/Scripts/Player.cs
@@ -8,6 +8,46 @@ using NaughtyAttributes;
 
 public class Player : MonoBehaviour
 {
+    [ReorderableList]
+    public List<GameObject> cameraHooks;
+    private GameObject mainCam = null;
+
+    private int prevHookIndx = 0;
+    private int currHookIndx = 0;
+
+    float fp = 1;
+
+    private void Start()
+    {
+        mainCam = GameObject.FindWithTag("MainCamera");
+
+        //setup everything for camera switching
+        currHookIndx = 0;
+        prevHookIndx = cameraHooks.Count;
+        if (cameraHooks.Count <= 0)
+        {
+            Debug.LogError("define camera hooks, you buffoon");
+        }
+        mainCam.transform.position = cameraHooks[0].transform.position;
+        mainCam.transform.rotation = cameraHooks[0].transform.rotation;
+    }
+
+    private void Update()
+    {
+        //Slerp position & rotation if not within certain margin
+        if (Vector3.Distance(mainCam.transform.position, cameraHooks[currHookIndx].transform.position) > 0.1f)
+        {
+            mainCam.transform.position = Vector3.Slerp(cameraHooks[prevHookIndx].transform.position, cameraHooks[currHookIndx].transform.position, fp);
+            fp += Time.deltaTime;
+        }
+
+        if (Quaternion.Angle(mainCam.transform.rotation, cameraHooks[currHookIndx].transform.rotation) > 2)
+        {
+            mainCam.transform.rotation = Quaternion.Slerp(cameraHooks[prevHookIndx].transform.rotation, cameraHooks[currHookIndx].transform.rotation, fp);
+            fp += Time.deltaTime;
+        }
+    }
+
     [Serializable]
     public struct TaskPair
     {
@@ -99,5 +139,31 @@ public class Player : MonoBehaviour
             default:
                 throw new ArgumentOutOfRangeException();
         }
+    }
+
+    public void NextTaskLocation()
+    {
+        Debug.Log("Next Phase");
+        if ((currHookIndx + 1) < cameraHooks.Count)
+        {
+            //safe
+            GoToPhase(currHookIndx + 1);
+        }
+        else
+        {
+            //wrap to first phase
+            GoToPhase(0);
+        }
+    }
+
+    /// <summary>
+    /// Public for any freaky boys who want to call this elsewhere
+    /// </summary>
+    /// <param name="phaseIndex"></param>
+    public void GoToPhase(int phaseIndex)
+    {
+        prevHookIndx = currHookIndx;
+        currHookIndx = phaseIndex;
+        fp = 0.1f;
     }
 }
