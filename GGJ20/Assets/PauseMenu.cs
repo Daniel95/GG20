0856c602de8402ab5e8d5c5d7b0c591b4a4b7577
thatoneguyleon <leonvdb27@hotmail.com>
2020-02-02 02:18:46 +0100

    Created a simple pause menu

---
+++ GGJ20/Assets/PauseMenu.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public static bool gameIsPaused = false;
+
+    [SerializeField,Tooltip("Displays the pause menu")]
+    public GameObject pauseMenuUI;
+
+    public  Button pauseMenuButton;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0)) //change this to button input
+        {
+             if (gameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
+}
