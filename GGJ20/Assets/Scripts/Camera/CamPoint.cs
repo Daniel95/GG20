9eb75ecaf427ddad820c2e080bfd9c081a42e57a
Max <max.mxmail@gmail.com>
2020-02-03 22:48:25 +0100

    sync

--- GGJ20/Assets/Scripts/Camera/CamPoint.cs
+++ GGJ20/Assets/Scripts/Camera/CamPoint.cs
@@ -5,4 +5,9 @@ using UnityEngine;
 public class CamPoint : MonoBehaviour
 {
     public WorkManager.TaskType taskType;
+
+    private void OnValidate()
+    {
+        this.name = "CameraPoint " + taskType.ToString();
+    }
 }
